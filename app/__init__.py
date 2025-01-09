f035232a37b5a302cc2632f4a76964bdd133381c
Jady Lei <jlei50@stuy.edu>
2025-01-09 11:02:11 -0500

    flask setup

---
+++ app/__init__.py
@@ -0,0 +1,89 @@
+## Quarter Collective: Ankita Saha, Evan Chan, and Jady Lei
+
+
+# imports
+import os
+import sqlite3
+import json
+import urllib.request
+import datetime
+import random
+
+from flask import Flask, render_template, redirect, session, request, flash, jsonify
+
+#custom module
+# from sitedb import *
+# from apis import *
+# from html_builder import *
+
+# flask App
+app = Flask(__name__, template_folder = "templates", static_folder = "../static")
+app.secret_key = os.urandom(32)
+
+@app.route("/")# checks for session and sends user to appropriate spot
+def checkSession():
+    # setup functions go here:
+    # createUsers()
+
+    return redirect("/home")
+    # if 'username' in session:
+    #     return redirect("/home")
+    # return redirect("/login")
+
+@app.route("/login", methods=["GET", "POST"])# will code registering and logging forms later
+def login():
+    if 'username' in session:
+        return redirect("/home")
+
+    if request.method =="POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username or not password:# checks if both form entries were filled out
+            flash("Missing username/password", "error")
+            return redirect("/login")
+
+        if checkPassword(username, password):# if password is correct, given user exists
+            session["username"] = username# adds user to session
+            return redirect("/home")
+
+        else:# if password isnt correct
+            flash("Invalid username/password", "error")
+            return redirect("/login")
+
+    return render_template("login.html")# if GET request, just renders login page
+
+@app.route("/register", methods=["GET", "POST"])# will code registering and logging forms later
+def register():
+    if request.method =="POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        city = request.form.get("city")
+
+        if not username or not password or not city:# checks if all 3 form entries were filled out
+            return render_template("register.html", warning = "empty field(s)")
+
+        #check if user has special chars
+        #check for existing username
+        message = addUser(username, password, city)
+        if message:
+            return render_template("register.html", warning = message)
+        else:
+            return redirect("/login")
+
+    return render_template("register.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/logout")
+def removeSession():
+    session.pop('username', None)
+    return redirect("/")
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
