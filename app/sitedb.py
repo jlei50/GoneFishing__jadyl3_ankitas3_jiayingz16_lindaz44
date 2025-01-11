dba734b4383183577020a551e10f71a72518cc09
Ankita Saha <sahaankita348@gmail.com>
2025-01-11 14:53:34 -0500

    actually adding gamesame functyions

--- app/sitedb.py
+++ app/sitedb.py
@@ -39,3 +39,39 @@ def deleteUsers():
     db = sqlite3.connect(USER_FILE)
     c = db.cursor()
     c.execute("DROP table userTable")
+
+def createGameSavesTable():
+    gameSaves = sqlite3.connect(USER_FILE)
+    c = gameSaves.cursor()
+    command = "CREATE TABLE IF NOT EXISTS gameSaves (username TEXT, day INT, food TEXT, money INT, progress INT, crewMood TEXT)"
+    c.execute(command)
+    gameSaves.commit()
+
+def addGameStats(username):
+    if (c.execute("SELECT 1 FROM userTable WHERE username=?", (username,))).fetchone() == None:
+        c.execute("INSERT INTO gameSaves (username, day, food, money, progress, crewMood) VALUES (?, ?, ?, ?, ?, ? (username, day, food, money, progress, crewMood))")
+        gameSaves.commit()
+    return "game stats added"
+
+def saveGame(username):
+    if (c.execute("SELECT 1 FROM userTable WHERE username=?", (username,))).fetchone() == None:
+        c.execute("UPDATE gameSaves set day=?, food=?, money=?, progress=?, crewMood=?, WHERE username=?", (day, food, money, progress, crewMood, username))
+        gameSaves.commit()
+    return "game stats added"
+
+def getGameStats(username):
+    if (c.execute("SELECT 1 FROM userTable WHERE username=?", (username,))).fetchone() == None:
+        c.execute("SELECT * FROM gameSaves set day=?, food=?, money=?, progress=?, crewMood=?, WHERE username=?", (day, food, money, progress, crewMood, username))
+        gameSaves.commit()
+        return c.fetchone()
+
+#]def addDay():
+
+# def addMoney():
+
+
+
+
+
+
+
