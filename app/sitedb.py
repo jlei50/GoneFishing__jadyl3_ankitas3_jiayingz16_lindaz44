47f20ed209730ec38edead7f33c5b7a0754c0144
Michelle <jzhu50@stuy.edu>
2025-01-14 10:02:56 -0500

    fixed bugs in site.db

--- app/sitedb.py
+++ app/sitedb.py
@@ -67,25 +67,25 @@ def deleteUsers():
 def createGameSavesTable():
     gameSaves = sqlite3.connect(USER_FILE)
     c = gameSaves.cursor()
-    command = "CREATE TABLE IF NOT EXISTS gameSaves (username TEXT, day INT, food INT, crew INT, progress REAL, crewMood TEXT)"
+    command = "CREATE TABLE IF NOT EXISTS gameSaves (username TEXT, day INT, food INT, money INT, progress REAL, crewMood TEXT)"
     c.execute(command)
     gameSaves.commit()
 
 #use when first adding stas for user
-def addGameStats(username, day, food, crew, progress, crewMood):
+def addGameStats(username, day, food, money, progress, crewMood):
     gameSaves = sqlite3.connect(USER_FILE)
     c = gameSaves.cursor()
     if (c.execute("SELECT 1 FROM userTable WHERE username=?", (username,))).fetchone():
-        c.execute("INSERT INTO gameSaves (username, day, food, crew, progress, crewMood) VALUES (?, ?, ?, ?, ?, ?)", (username, day, food, crew, progress, crewMood))
+        c.execute("INSERT INTO gameSaves (username, day, food, money, progress, crewMood) VALUES (?, ?, ?, ?, ?, ?)", (username, day, food, money, progress, crewMood))
         gameSaves.commit()
     return "game stats added"
 
 #use once user stats in database
-def saveGame(username, day, food, crew, progress, crewMood):
+def saveGame(username, day, food, money, progress, crewMood):
     gameSaves = sqlite3.connect(USER_FILE)
     c = gameSaves.cursor()
     if (c.execute("SELECT 1 FROM userTable WHERE username=?", (username,))).fetchone():
-        c.execute("UPDATE gameSaves set day=?, food=?, crew=?, progress=?, crewMood=? WHERE username=?", (day, food, crew, progress, crewMood, username))
+        c.execute("UPDATE gameSaves set day=?, food=?, money=?, progress=?, crewMood=? WHERE username=?", (day, food, money, progress, crewMood, username))
         gameSaves.commit()
     return "game stats added"
 
